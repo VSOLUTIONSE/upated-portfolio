25c8629d5a5ab6eb97330ed8a6375b0de7bb22df
Favour Emmanuel <femmaje@gmail.com>
2025-07-12 12:30:36 +0100

    restructure

--- components/works/Google.tsx
+++ components/works/Google.tsx
@@ -14,31 +14,33 @@ const Google = () => {
       </h3>
       <p className="mt-1 text-sm font-medium text-textDark">
       March 2024 - July 2024, Nigeria
-      </p>
-      <ul className="flex flex-col gap-3 mt-6">
-        <li className="flex gap-2 text-base text-textDark">
-          <span className="mt-1 text-textGreen">
-            <TiArrowForward />
-          </span>
-          Transforming Figma designs into sleek, functional, and pixel-perfect
-          interfaces for Blockchain Decentralized Applications (dApps), ensuring
-          seamless user experiences.
-        </li>
-        <li className="flex gap-2 text-base text-textDark">
-          <span className="mt-1 text-textGreen">
-            <TiArrowForward />
-          </span>
-          Building and maintaining a scalable design system, creating reusable
-          UI components that enhance consistency and efficiency across projects.
-        </li>
-        <li className="flex gap-2 text-base text-textDark">
-          <span className="mt-1 text-textGreen">
-            <TiArrowForward />
-          </span>
-          Working closely with fellow developers, leveraging version control
-          (Git & GitHub) to maintain clean, efficient, and scalable codebases.
-        </li>
-      </ul>
+      </p><ul className="flex flex-col gap-3 mt-6">
+              <li className="flex gap-2 text-base text-textDark">
+                <span className="mt-1 text-textGreen">
+                  <TiArrowForward />
+                </span>
+                Built intuitive, pixel-perfect user interfaces for decentralized
+                applications using Next.js, TypeScript, and Tailwind CSS.
+              </li>
+              <li className="flex gap-2 text-base text-textDark">
+                <span className="mt-1 text-textGreen">
+                  <TiArrowForward />
+                </span>
+                Refactored and optimized a legacy codebase, reducing load times by
+                20%. Developed and integrated new features into pre-existing
+                blockchain apps, improving user engagement by 15%.
+              </li>
+              <li className="flex gap-2 text-base text-textDark">
+                <span className="mt-1 text-textGreen">
+                  <TiArrowForward />
+                </span>
+                Designed and implemented a scalable design system, ensuring consistent
+                branding across all products. Collaborated cross-functionally with
+                developers, product designers, and project leads to meet tight project
+                deadlines.
+              </li>
+            </ul>
+      
     </motion.div>
   );
 };
